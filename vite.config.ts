c569b8f1d185e9426cbd69e37dd4a3972a0ae608
ser2331 <dukov_serega@mail.com>
2025-11-27 18:08:34 +0300

    main: some fixes add api

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH,
+  base: '/max-medical-booking/',
 
   server: {
     host: '0.0.0.0',
